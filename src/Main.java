03ad201679344cca33b49fc59ff953797229c2af
Dima Lis <iselis777888999@gmail.com>
2023-07-13 16:49:40 +0300

    second commit reformate code

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@
 
 public class Main {
     public static void main(String[] args) {
-    int ticketCost = 100_000; // Значение стоимости одного авиабилета
-    int bonus = 20; // Значение количества рублей для одной бонусной мили
-    int miles = ticketCost/bonus; // Количество бонусных миль
+        int ticketCost = 100_000; // Значение стоимости одного авиабилета
+        int bonus = 20; // Значение количества рублей для одной бонусной мили
+        int miles = ticketCost / bonus; // Количество бонусных миль
 
 
-        System.out.println ("+--------------------------+");
-        System.out.println ("| Количество бонусных миль |"); //+ miles = ticketCost/bonus); // Расчет кличества бонусных миль исходя из стоимости одного авиабилета
-        System.out.println ("|          " + miles + "            |");
-        System.out.println ("+--------------------------+");
+        System.out.println("+--------------------------+");
+        System.out.println("| Количество бонусных миль |"); //+ miles = ticketCost/bonus); // Расчет кличества бонусных миль исходя из стоимости одного авиабилета
+        System.out.println("|          " + miles + "            |");
+        System.out.println("+--------------------------+");
 
     }
 }
